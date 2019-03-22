07f876005cd565ab7bfdf5fbc2f90d5a5f71f3c8
17kikak <kikakovaleski@gmail.com>
2019-03-22 17:54:11 -0400

    added artist_id, made albums belong to artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190316004000) do
+ActiveRecord::Schema.define(version: 20190322210306) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190316004000) do
     t.string "album_cover_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
